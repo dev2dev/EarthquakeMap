dcc8b19fc0b755fef30e7d6482b5b9798ae7db5c
Matt Christiansen <matt@nikore.net>
2010-05-06 11:36:38 -0700

    Added a title to the webview window

--- Classes/EarthquakeWebViewController.h
+++ Classes/EarthquakeWebViewController.h
@@ -4,14 +4,14 @@
 
 @interface EarthquakeWebViewController : UIViewController <UIWebViewDelegate> {
 	IBOutlet UIWebView *webView;
-	IBOutlet UIToolbar *toolbar;
+	IBOutlet UINavigationBar *toolbar;
 	IBOutlet UIBarButtonItem *closeButton;
 	Earthquake *earthquake;
 	
 }
 
 @property (nonatomic, retain) IBOutlet UIWebView *webView;
-@property (nonatomic, retain) IBOutlet UIToolbar *toolbar;
+@property (nonatomic, retain) IBOutlet UINavigationBar *toolbar;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *closeButton;
 @property (nonatomic, retain) Earthquake *earthquake;
 
--- Classes/EarthquakeWebViewController.m
+++ Classes/EarthquakeWebViewController.m
@@ -16,6 +16,20 @@
 
 - (void) setLabels {
 	[webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:earthquake.USGSWebLink]]];
+	UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10,10,200,30)];
+	titleLabel.text = earthquake.location;
+	titleLabel.backgroundColor = [UIColor clearColor];
+	UINavigationItem *navItem = [UINavigationItem alloc];
+	navItem.titleView = titleLabel;
+	
+	closeButton = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:nil action:@selector(closeWindow:)];
+	navItem.rightBarButtonItem = closeButton;
+	
+	[toolbar pushNavigationItem:navItem animated:NO];
+	[toolbar setDelegate:self];
+	[navItem release];
+	[titleLabel release];
+	
 }
 
 @end
